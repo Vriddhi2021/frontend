8a52bccaa74dd6ebcca9da254ba881207f4baab2
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-03-15 14:06:21 +0530

    team page done

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,14 @@
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="col-10 col-md-4 mx-auto">
-            <div className="card">
+            <div className="card my-4">
             <div className="img-bx">
-                <img src="https://i.postimg.cc/dQ7zWbS5/img-4.jpg" alt="img" />
+                <img src={props.img} alt="img" />
             </div>
             <div className="content">
                 <div className="detail">
-                    <h2 >Emilia Roy<br/><span>Senior Designer</span></h2><p>Core Team</p>
+                    <h2 >{props.name}<br/><span></span></h2><p>{props.post}</p>
                 </div>
             </div>
         </div>
--- pages/team.js
+++ pages/team.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 import bgHero from '../public/gellery.png';
 import Hero from '../components/hero';
 import TrendingVideo from '../components/trendingVideo';
-import Grid from '@mui/material/Grid';
+import teamDetails from '../components/teamDetails';
 import Footer from '../components/footer';
 
 
@@ -17,10 +17,176 @@ const team = () => {
       />
       <TrendingVideo />
       <section>
-        <div className="row">
+        <div className="row mt-5">
           <div className="col-11 col-md-10 mx-auto">
-            <div className="row">
-            <Card/><Card/><Card/>
+          <div className="row">
+          <h2 className="teamPageHeading text-white">Conveners</h2>
+            {teamDetails.filter((old) => (old.post === "Convener")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">Tech Team</h2>
+            {teamDetails.filter((old) => (old.team === "Tech Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">Content Team</h2>
+            {teamDetails.filter((old) => (old.team === "Content Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">Design Team</h2>
+            {teamDetails.filter((old) => (old.team === "Design Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">DBM Team</h2>
+            {teamDetails.filter((old) => (old.team === "DBM Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">Publicity Team</h2>
+            {teamDetails.filter((old) => (old.team === "Publicity Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">Sponsership Team</h2>
+            {teamDetails.filter((old) => (old.team === "Sponsership Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">PR Team</h2>
+            {teamDetails.filter((old) => (old.team === "PR Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">EM Team</h2>
+            {teamDetails.filter((old) => (old.team === "EM Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">IA Team</h2>
+            {teamDetails.filter((old) => (old.team === "IA Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">CM Team</h2>
+            {teamDetails.filter((old) => (old.team === "CM Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">Streaming Team</h2>
+            {teamDetails.filter((old) => (old.team === "Streaming Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
+            </div>
+            <div className="row my-4">
+            <h2 className="teamPageHeading text-white">Workshop Team</h2>
+            {teamDetails.filter((old) => (old.team === "Workshop Team")).map((oldData) => {
+              return <Card
+                key={oldData.id}
+                id={oldData.id}
+                name={oldData.name}
+                post={oldData.post}
+                team={oldData.team}
+                img={oldData.img}
+              />
+            })}
             </div>
           </div>
         </div>
