a567b86fbbd4b1f69943d5a9c122e9608f6a510f
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-03-15 08:54:43 +0530

    team page added

---
+++ components/teamDetails.js
@@ -0,0 +1,20 @@
+import img1 from '../public/game.png';
+
+const TeamDetails = [
+    {
+        id: 1,
+        name: "Sahil",
+        post: "Core Team",
+        team: "Tech Team",
+        img: img1.src
+    },
+    {
+        id: 2,
+        name: "Shailesh",
+        post: "Core Team",
+        team: "Tech Team",
+        img: img1.src
+    }
+]
+
+export default TeamDetails;
--- pages/team.js
+++ pages/team.js
@@ -1,9 +1,29 @@
+import Card from '../components/Card';
+import Navbar from '../components/Navbar';
+import bgHero from '../public/gellery.png';
+import Hero from '../components/hero';
+import TrendingVideo from '../components/trendingVideo';
+import Footer from '../components/footer';
+
 
 const team = () => {
   return (
+    <>
+     <Navbar/>
+     <Hero
+        title="Our Team"
+        img={bgHero.src}
+     />
+    <TrendingVideo/>
     <section>
-        Team
+    <section>
+    <Card/>
+    <Card/>
+    <Card/>
     </section>
+    </section>
+    <Footer/>
+     </>
   )
 }
 
