9b94af5b0589cb6cb547a5c27632f718ee993008
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-03-15 11:00:21 +0530

    card

---
+++ pages/team.js
@@ -0,0 +1,28 @@
+import Card from '../components/Card';
+import Navbar from '../components/Navbar';
+import bgHero from '../public/gellery.png';
+import Hero from '../components/hero';
+import TrendingVideo from '../components/trendingVideo';
+import Footer from '../components/footer';
+
+
+const team = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        title="Our Team"
+        img={bgHero.src}
+      />
+      <TrendingVideo />
+      <section>
+        <section>
+          <Card />
+          <Card />
+          <Card />
+        </section>
+      </section>
+      <Footer />
+    </>
+  )
+}
