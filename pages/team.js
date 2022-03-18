c1707b53d009f3d4ef4627ee67bce001c001e4d8
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-03-18 15:49:11 +0530

    meta tag added

--- pages/team.js
+++ pages/team.js
@@ -1,6 +1,7 @@
 import Card from '../components/Card';
 import Navbar from '../components/Navbar';
 import bgHero from '../public/team.png';
+import Head from "next/head";
 import Hero from '../components/hero';
 import TrendingVideo from '../components/trendingVideo';
 import teamDetails from '../components/teamDetails';
@@ -10,6 +11,27 @@ import Footer from '../components/footer';
 const team = () => {
   return (
     <>
+    <Head>
+        <title>Vriddhi - Our Team</title>
+
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+
+        {/* *********** Meta SEO***********  */}
+        <meta property="og:title" content="Vriddhi'21, E-sports is a place for all of you, so don't hold back. Embrace the gamer in you as you will have the most fantastic time during Vriddhi 2021 this year" />
+        <meta name="keywords"  content="vriddhinitr,vriddhinitrkl,vriddhinitrourkela, vriddhi, fest nitr, fest, festnitr, festnitrkl, nitr,nitrkl,nitrourkela,nit,rourkela,nit rourkela,esports fest,esports" />
+        <meta name="description" content="Official Website of Vriddhi NIT Rourkela" />
+        
+        {/* *********** Meta Facebook SEO***********  */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.vriddhinitr.com/" />
+        <meta property="og:image" content="https://www.vriddhinitr.com/_next/static/media/logoBranding.00987fe3.png" />
+        <meta property="og:image:width" content="806" />
+        <meta property="og:image:height" content="280" /> <meta name="description" content="Vriddhi 2021" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Navbar />
       <Hero
         title="Our Team"
