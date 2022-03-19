64d1ee6162bd2c1f0b52eda0a685870c468fbce8
Aditya <Adi72Hzb@gmail.com>
2022-03-19 11:55:58 +0530

    prevent default

--- components/registerUser.js
+++ components/registerUser.js
@@ -24,7 +24,7 @@ const RegisterUser = () => {
     setPerson({ ...person, [name]: !isNitr });
   };
   const handleSubmit = async (e) => {
-    //e.preventDefault();
+    e.preventDefault();
     function getCookie(cname) {
       let name = cname + "=";
       let decodedCookie = decodeURIComponent(document.cookie);
