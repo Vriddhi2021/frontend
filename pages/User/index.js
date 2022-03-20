80cc43acc3ac14c9476cdd04a8a518fdd633efc8
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-03-21 03:23:48 +0530

    index user modified

--- pages/User/index.js
+++ pages/User/index.js
@@ -1,6 +1,6 @@
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/footer';
-import Hero from '../components/hero';
+// import Hero from '../components/hero';
 import RegisterUser from '../../components/registerUser';
 import JoinusHero from '../../components/joinusHero';
 
